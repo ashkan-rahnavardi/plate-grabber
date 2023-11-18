cffd7fb9dc0e88c9153acf08be97cbfca0f271ca
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-11-18 12:27:04 -0800

    rear-facing camera takes photos & displays photo

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,22 +1,37 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const Camera: React.FC = () => {
 	const webcamRef = useRef<Webcam>(null);
+	const [capturedImage, setCapturedImage] = useState<string | null>(null);
 
-	const capture = () => {
+	const videoConstraints = {
+		facingMode: { exact: 'environment' },
+	};
+
+	const capture = useCallback(() => {
+		// const imageSrc = webcamRef.current?.getScreenshot();
+		// setCapturedImage(imageSrc);
 		if (webcamRef.current) {
 			const imageSrc = webcamRef.current.getScreenshot();
-			// Do something with the imageSrc, like sending it to a server or updating state
+			setCapturedImage(imageSrc);
 		}
-	};
+	}, [webcamRef]);
 
 	return (
 		<div>
-			<Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+			<Webcam
+				audio={false}
+				ref={webcamRef}
+				screenshotFormat="image/jpeg"
+				width={640}
+				height={480}
+				videoConstraints={videoConstraints}
+			/>
 			<button onClick={capture}>Capture photo</button>
+			{capturedImage && <img src={capturedImage} alt="Captured" />}
 		</div>
 	);
 };
