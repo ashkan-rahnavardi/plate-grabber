6cc0ddd799f2b1982c4d0034e37f2c9ec5383bc0
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-12-12 09:06:54 -0800

    added title to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<title>Plate Grabber</title>
+			</head>
 			<body className={inter.className}>
 				<StorageProvider>{children}</StorageProvider>
 			</body>
