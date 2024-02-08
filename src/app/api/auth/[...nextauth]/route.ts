8014edfeb2de1b146c1dbeed041c82fbfcde7a11
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2024-02-08 00:33:33 -0800

    should host properly

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,2 @@
-export { GET, POST } from '@/services/auth';
+import { handlers } from '@/services/auth';
+export const { GET, POST } = handlers;
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,18 +1,31 @@
 import clientPromise from '@/database/mongodb';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import type { NextAuthConfig } from 'next-auth';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const {
-	handlers: { GET, POST },
-	auth,
-} = NextAuth({
+export const config = {
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
 		}),
 	],
-	// debug: process.env.NODE_ENV === 'development',
 	adapter: MongoDBAdapter(clientPromise),
-});
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
+
+// export const config = {
+// 	handlers: { GET, POST },
+// 	auth,
+// } = NextAuth({
+// 	providers: [
+// GoogleProvider({
+// 	clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+// 	clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+// }),
+// 	],
+// 	// debug: process.env.NODE_ENV === 'development',
+// 	adapter: MongoDBAdapter(clientPromise),
+// });
