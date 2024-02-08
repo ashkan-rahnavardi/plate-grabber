7fd72374ab1bf400b0e3e5990b48c8e87ebcb467
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2024-02-08 01:11:35 -0800

    hosted auth should work

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,12 +5,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const config = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-		}),
-	],
+	providers: [GoogleProvider],
 	adapter: MongoDBAdapter(clientPromise),
 } satisfies NextAuthConfig;
 
