cf2a3f1dd425c6790ea280f1be1be1d1d2cebcad
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2024-03-02 14:00:13 -0800

    removed commented out code

--- src/containers/Form/index.tsx
+++ src/containers/Form/index.tsx
@@ -2,16 +2,17 @@
 
 import { FormsContext } from '@/services/FormsProvider';
 import { useParams } from 'next/navigation';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 export default function Form() {
 	const params = useParams();
 	const forms = useContext(FormsContext);
+	const [form, setForm] = useState(
+		forms.find((form) => form.reference === params.id)
+	);
 
 	console.log('params from form/index', params.id);
-	console.log('forms from form/index', forms);
+	console.log('form from form/index', form);
 
 	return <div>Hello</div>;
 }
-
-// { params }: { params: { id: string } }
