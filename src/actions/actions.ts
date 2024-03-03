4accc611a7f1775687ec88c87e274d780e61d44b
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2024-03-02 16:15:07 -0800

    cleaned up server action imports

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,40 +1,7 @@
 'use server';
 import dbConnect from '@/database/dbConnect';
-import { authOptions } from '@/lib/authOptions';
 import FormModel from '@/models/form';
-import UserModel from '@/models/user';
 import { LicenseForm } from '@/types/licenseForm';
-import { UserSession } from '@/types/userSession';
-import { getServerSession } from 'next-auth/next';
-
-export async function Save(formData: any) {
-	await dbConnect();
-	const session = await getServerSession(authOptions);
-	const form = await FormModel.findOneAndUpdate(
-		{ Reference: formData.Reference },
-		formData
-	);
-
-	if (session) {
-		const user = await UserModel.findOne({ email: session.user?.email ?? '' });
-		if (form) {
-			console.log('found and updated');
-		} else {
-			formData.Email = session.user?.email ?? '';
-			const newForm = new FormModel(formData);
-			try {
-				await newForm.save();
-				user.forms.push(newForm._id);
-				await user.save();
-				console.log('saved new form');
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	} else {
-		console.log('no session');
-	}
-}
 
 export async function saveForm(formData: LicenseForm) {
 	await dbConnect();
@@ -86,3 +53,32 @@ export async function GetForm(id: string) {
 	const form = await FormModel.findById(id);
 	return form;
 }
+
+// export async function Save(formData: any) {
+// 	await dbConnect();
+// 	const session = await getServerSession(authOptions);
+// 	const form = await FormModel.findOneAndUpdate(
+// 		{ Reference: formData.Reference },
+// 		formData
+// 	);
+
+// 	if (session) {
+// 		const user = await UserModel.findOne({ email: session.user?.email ?? '' });
+// 		if (form) {
+// 			console.log('found and updated');
+// 		} else {
+// 			formData.Email = session.user?.email ?? '';
+// 			const newForm = new FormModel(formData);
+// 			try {
+// 				await newForm.save();
+// 				user.forms.push(newForm._id);
+// 				await user.save();
+// 				console.log('saved new form');
+// 			} catch (err) {
+// 				console.log(err);
+// 			}
+// 		}
+// 	} else {
+// 		console.log('no session');
+// 	}
+// }
